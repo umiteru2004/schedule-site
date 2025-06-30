4829a9150c5774a522d2f6df352f7d69b0c55fd6
shunezaki <shunezaki1206@gmail.com>
2025-06-30 14:05:57 +0900

    autoputto wo henkou

--- php/post-exam-output.php
+++ php/post-exam-output.php
@@ -1,3 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>過去問投稿応答ページ</title>
+<style>
+    body {
+        font-family: sans-serif;
+        background-color: #e0e0e0;
+        color: #333;
+        padding: 20px;
+    }
+</style>
+</head>
+
+<body>
 <?php
 $pdo=new PDO('mysql:host=localhost;dbname=exams_site;charset=utf8','staff','password');
 $subject = $_REQUEST['subject'] ?? '';
@@ -7,6 +24,7 @@ if (isset($_FILES['image']) && $_FILES['image']['error'] === UPLOAD_ERR_OK) {
     $file_content = file_get_contents($_FILES['image']['tmp_name']);
 } else {
     echo 'ファイルのアップロードに失敗したか、ファイルが選択されていません';
+    exit();
 }
 
 $sql=$pdo->prepare('insert into exam(subject, type, image) values(?, ?, ?)');
@@ -16,3 +34,5 @@ if($sql->execute([$subject, $type, $file_content])){
     echo '追加できませんでした';
 }
 ?>
+</body>
+</html>
