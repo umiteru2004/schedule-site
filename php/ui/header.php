cd93b750a6dcc4b685603db9910b7eb599b2bba4
umiteru2004 <umiteru2004@gmail.com>
2025-07-01 10:43:57 +0900

    feat: Google FontsからヘッダーにNoto Sans JPを読み込みました。

--- php/ui/header.php
+++ php/ui/header.php
@@ -11,6 +11,10 @@ if (session_status() === PHP_SESSION_NONE) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo isset($page_title) ? htmlspecialchars($page_title, ENT_QUOTES, 'UTF-8') : 'My Website'; ?></title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
     <link href="/css/globals.css" rel="stylesheet" />
 </head>
 
