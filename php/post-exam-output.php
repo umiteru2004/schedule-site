7fb2534fcdbde04703c457a3d1718ad085459517
umiteru2004 <umiteru2004@gmail.com>
2025-07-15 10:02:49 +0900

    feat: 投稿結果ページをスタイリングしました。

--- php/post-exam-output.php
+++ php/post-exam-output.php
@@ -13,36 +13,30 @@ require "./ui/head.php";
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>過去問投稿応答ページ</title>
-        <style>
-            body {
-                font-family: sans-serif;
-                background-color: #e0e0e0;
-                color: #333;
-                padding: 20px;
-            }
-        </style>
     </head>
 
     <body>
-        <?php
-        $pdo = new PDO('mysql:host=localhost;dbname=exams_site;charset=utf8', 'staff', 'password');
-        $subject = $_REQUEST['subject'] ?? '';
-        $type = $_REQUEST['type'] ?? '';
-        $file_content = null;
-        if (isset($_FILES['image']) && $_FILES['image']['error'] === UPLOAD_ERR_OK) {
-            $file_content = file_get_contents($_FILES['image']['tmp_name']);
-        } else {
-            echo 'ファイルのアップロードに失敗したか、ファイルが選択されていません';
-            exit();
-        }
+        <main>
+            <?php
+            $pdo = new PDO('mysql:host=localhost;dbname=exams_site;charset=utf8', 'staff', 'password');
+            $subject = $_REQUEST['subject'] ?? '';
+            $type = $_REQUEST['type'] ?? '';
+            $file_content = null;
+            if (isset($_FILES['image']) && $_FILES['image']['error'] === UPLOAD_ERR_OK) {
+                $file_content = file_get_contents($_FILES['image']['tmp_name']);
+            } else {
+                echo 'ファイルのアップロードに失敗したか、ファイルが選択されていません';
+                exit();
+            }
 
-        $sql = $pdo->prepare('insert into exam(subject, type, image) values(?, ?, ?)');
-        if ($sql->execute([$subject, $type, $file_content])) {
-            echo '追加できました！！';
-        } else {
-            echo '追加できませんでした';
-        }
-        ?>
+            $sql = $pdo->prepare('insert into exam(subject, type, image) values(?, ?, ?)');
+            if ($sql->execute([$subject, $type, $file_content])) {
+                echo '追加できました！！';
+            } else {
+                echo '追加できませんでした';
+            }
+            ?>
+        </main>
     </body>
 
     </html>
