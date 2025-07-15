564339e788b4e17f65af4afc643bac5b1bbd8d04
umiteru2004 <umiteru2004@gmail.com>
2025-07-15 09:22:03 +0900

    feat: ホーム、教科、投稿ページで、共通の教科の配列を使用するようにしました。

--- php/index.php
+++ php/index.php
@@ -16,13 +16,7 @@
 
                 <tbody>
                     <?php
-                    $subjects = [
-                        'applied_math_b1' => '応用数学BⅠ',
-                        'general_english_b1' => '総合英語BⅠ',
-                        'signal_processing_1' => '信号処理Ⅰ',
-                        'network_application' => 'ネットワーク応用',
-                        'system_control_exercises' => 'システム制御演習'
-                    ];
+                    require "./lib/subjects.php";
 
                     foreach ($subjects as $key => $value) {
                         echo '<tr><td  class="subject-td">';
